f3d1b3fb4fe6b8156fdf33c188e58f6e68256a50
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:06:25 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "sentinel_data_connector_threat_intelligences" {
+  description = <<EOT
+Map of sentinel_data_connector_threat_intelligences, attributes below
+Required:
+    - log_analytics_workspace_id
+    - name
+Optional:
+    - lookback_date
+    - tenant_id
+EOT
+
+  type = map(object({
+    log_analytics_workspace_id = string
+    name                       = string
+    lookback_date              = optional(string, "01/01/1970 00:00:00")
+    tenant_id                  = optional(string)
+  }))
+}
+
